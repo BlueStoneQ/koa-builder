2d63c9054e629db3925e957c4753c0aeda1b1aff
qiaoyang <qiaoyang@bonc.com.cn>
2018-12-06 14:42:41 +0800

    基础设施-静态资源-ver.0

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
+const path = require('path');
 const Koa = require('koa');
+const static = require('koa-static');
 const router = require('./router');
 
 const app = new Koa();
-
-// PORT 端口号 -- 后面放到配置文件中
+// PORT 端口号 -- 后面放到配置文件
 const PORT = 3000;
+
+// 静态资源
+app.use(static(path.join(__dirname, 'public')));
 
 // 路由
 app
