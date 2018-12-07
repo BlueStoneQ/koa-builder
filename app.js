47dd0f28709c480dbd92b148b697804d0099da70
qiaoyang <qiaoyang@bonc.com.cn>
2018-12-07 10:25:13 +0800

    基础模块-bodyParser

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const path = require('path');
 const Koa = require('koa');
 const static = require('koa-static');
 const views = require('koa-views');
+const bodyParser = require('koa-bodyparser');
 const router = require('./router');
 
 const app = new Koa();
@@ -13,9 +14,17 @@ app.use(static(path.join(__dirname, 'public')));
 
 /**
 * 配置服务端模板渲染引擎中间件 -- 一定要在任何路由之前使用
+* (注意很多中间件都要在路由之前使用 不然在路由到某个业务时 该中间件还未加载)
 */
 app.use(views(path.join(__dirname, 'views'), {
   extension: 'ejs'
+}));
+
+/**
+ * 处理post请求中的body
+ */
+app.use(bodyParser({
+  formLimit: '1mb'
 }));
 
 // 路由
