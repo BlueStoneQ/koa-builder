142cfb577328ce668dbeef1755b566f143c26839
qiaoyang <qiaoyang@bonc.com.cn>
2019-01-17 18:38:36 +0800

    mysql-修正错误的配置信息

--- config/config.js
+++ config/config.js
@@ -12,11 +12,11 @@
   // DB-REDIS配置
   // DB-MYSQL数据库配置
   MYSQL: {
-    host: '47.105.62.98', // 数据库主机地址
-    port: '3306', // 数据库使用的端口
-    user: 'root', // 访问用户
-    password: 'man/Will179', // 访问用户对应的密码
-    database: 'maimai_test' // 数据库名称--后面改成麦麦
+    HOST: '47.105.62.98', // 数据库主机地址
+    PORT: '3306', // 数据库使用的端口
+    USER: 'manager', // 访问用户
+    PASSWORD: 'man/Will179', // 访问用户对应的密码
+    DATABASE: 'maimai_test' // 数据库名称--后面改成maimai
   }
  }
 
@@ -27,16 +27,16 @@
   // DB-REDIS配置
   // DB-MYSQL数据库配置
   MYSQL: {
-    DATABASE: 'mysql0531',
-    USER: 'root',
-    PASSWORD: '123456',
-    PORT: '3306',
-    HOST: 'localhost'
-    // host: '47.105.62.98', // 数据库主机地址
-    // port: '3306', // 数据库使用的端口
-    // user: 'root', // 访问用户
-    // password: 'man/Will179', // 访问用户对应的密码
-    // database: 'maimai_test' // 数据库名称--后面改成麦麦
+    // DATABASE: 'mysql0531',  // 本地mysql 8.0
+    // USER: 'root',
+    // PASSWORD: '123456',
+    // PORT: '3306',
+    // HOST: 'localhost'
+    HOST: '47.105.62.98', // 数据库主机地址
+    PORT: '3306', // 数据库使用的端口
+    USER: 'manager', // 访问用户
+    PASSWORD: 'man/Will179', // 访问用户对应的密码
+    DATABASE: 'maimai_test' // 数据库名称--后面改成maimai
   }
  }
 
--- controller/signUp.js
+++ controller/signUp.js
@@ -6,7 +6,6 @@ exports.signup = async ctx => {
   // 可以查询数据 然后在这里打印测试下哈哈
   await userModel.list()
     .then(async (result) => {
-      console.log(`从用户表中查到的信息：${JSON.stringify(result)}`);
       ctx.body = `从用户表中查到的信息：${JSON.stringify(result)}`;
     });
 }
